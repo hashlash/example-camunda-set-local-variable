bbeb80516c184e2d3c4cb3d396ece5d3c8084ebc
hashlash <muh.ashlah@gmail.com>
2017-08-08 19:59:32 +0700

    Initial commit

---
+++ src/main/java/example/camunda/delegate/InitLocalVar.java
@@ -0,0 +1,15 @@
+package example.camunda.delegate;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.ExecutionListener;
+
+/**
+ * Created by ashlah on 08/08/17.
+ */
+public class InitLocalVar implements ExecutionListener {
+    @Override
+    public void notify(DelegateExecution execution) throws Exception {
+        execution.setVariable("myVar", "a value", execution.getCurrentActivityId());
+        execution.setVariable("myVar", "a value"); // this work fine
+    }
+}
