392e581decf36640db87af5c895403444e91fe6f
hashlash <muh.ashlah@gmail.com>
2017-08-09 12:19:43 +0700

    update for testing process engine exception
    when trying to get incidents from camunda's cockpit

--- src/main/java/example/camunda/delegate/InitLocalVar.java
+++ src/main/java/example/camunda/delegate/InitLocalVar.java
@@ -9,7 +9,7 @@ import org.camunda.bpm.engine.delegate.ExecutionListener;
 public class InitLocalVar implements ExecutionListener {
     @Override
     public void notify(DelegateExecution execution) throws Exception {
-        execution.setVariable("myVar", "a value", execution.getCurrentActivityId());
+//        execution.setVariable("myVar", "a value", execution.getCurrentActivityId());
         execution.setVariable("myVar", "a value"); // this work fine
     }
 }
